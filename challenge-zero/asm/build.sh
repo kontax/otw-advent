aa89983e94520b1011f2f7012c42b6d8f15b262e
James Coulson <github@coulson.ie>
2019-11-08 08:45:45 +0000

    Clean up locations

--- challenge-zero/asm/build.sh
+++ challenge-zero/asm/build.sh
@@ -1,2 +1,3 @@
 nasm -f elf32 -o encr.o encr.s && ld -m elf_i386 -o encr encr.o && rm encr.o
 nasm -f elf32 -o decr.o decr.s && ld -m elf_i386 -o decr decr.o && rm decr.o
+nasm -f elf32 -o solve.o solve.s && ld -m elf_i386 -o solve solve.o && rm solve.o
