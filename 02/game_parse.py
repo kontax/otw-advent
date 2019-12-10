6da12fd513d5df2ae1f02fe75f748fa9da63a34b
James Coulson <github@coulson.ie>
2019-12-10 16:51:00 +0000

    Complete parser

--- 02/game_parse.py
+++ 02/game_parse.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 import hexdump
 import select
+import signal
 import socket
+import sys
+import json
 from key import KEY
 from io import BytesIO
+from traceback import print_exception
 
 
 HOST = '3.93.128.89'
@@ -15,14 +19,19 @@ BUFFER = 4096   # Number of bytes to send/receive at a time
 CLI_IDX = 0
 SRV_IDX = 0
 
-LOOKUP = {
-    'lvl_up': 0x08,
-    'xp'    : 0x10,
-    'max_xp': 0x18,
-    'lv'    : 0x20,
-    'hp'    : 0x28,
-    'max_hp': 0x30,
-    'g'     : 0x38
+SUMMARY_LOOKUP = {
+    0x08: 'lvl_up',
+    0x10: 'xp',
+    0x18: 'max_xp',
+    0x20: 'lv',
+    0x28: 'hp',
+    0x30: 'max_hp',
+    0x38: 'g'
+}
+
+LOCATION = {
+    0x01: 'stash',
+    0x02: 'shop'
 }
 
 def twos_comp(val, bits):
@@ -39,122 +48,213 @@ def num_convert(stream):
         num = get_int(stream, 1)
         ret |= ((num & 0x7f) << shift)
         shift += 7
-        is_negative = num >= 0x7f
+        is_negative = num > 0x7f
 
     return twos_comp(ret, 64)
 
 def get_int(stream_bytes, num):
     return int.from_bytes(stream_bytes.read(num), 'little')
 
+def print_error(stream, ex):
+        print("---- ERROR DETAILS ---")
+        print_exception(type(ex), ex, sys.exc_info()[2])
+        print(f"{ex} @ {stream.tell()}")
+        stream.seek(stream.tell()-2)
+        print(hexdump.hexdump(stream.read()))
+        stream.seek(0)
+        print(hexdump.hexdump(stream.read()))
+
 
 def parse(stream):
-    print(hexdump.hexdump(stream))
     stream_bytes = BytesIO(stream)
     stream_length = get_int(stream_bytes, 2)
     event = get_int(stream_bytes, 1)
 
     if event == 0x12:
         # Enemy 
-        return parse_attack(stream_bytes)
+        try:
+            return parse_attack(stream_bytes)
+        except Exception as ex:
+            print_error(stream_bytes, ex)
+            return {}
     
     elif event == 0x1a:
         # Buy
-        return buy_item(stream_bytes)
+        try:
+            return {'items': shop_item(stream_bytes)}
+        except Exception as ex:
+            print_error(stream_bytes, ex)
+            return {}
+
+    elif event == 0x0a:
+        # Initialization
+        try:
+            return initialize(stream_bytes)
+        except Exception as ex:
+            print_error(stream_bytes, ex)
+            return {}
+
+    elif event == 0x22:
+        # Use
+        try:
+            return use(stream_bytes)
+        except Exception as ex:
+            print_error(stream_bytes, ex)
+            return {}
 
     else:
         return hexdump.hexdump(stream)
 
-def buy_item(stream):
+def initialize(stream):
     stream_length = num_convert(stream)
+    if stream_length == 0:
+        return {}
+
+    assert stream.read(2) == b'\x08\x01'
+    summary_details = get_summary_details(stream)
 
     item_details = []
-    item_details.append({'length': stream_length})
-    item_details.append({'storage': get_items(stream)})
-    item_details.append({'storage': get_items(stream)})
-    item_details.append({'other': stream.read()})
+    while get_int(stream, 1) == 0x1a:
+        item_details.append(get_item_details(stream))
 
-    return {'items': item_details}
+    return {
+        'length': stream_length,
+        'summary': summary_details,
+        'items': item_details
+    }
+
+def shop_item(stream):
+    stream_length = num_convert(stream)
+    shop_items = {}
+    shop_items['length'] = stream_length
+
+    item_details = []
+    assert get_int(stream, 1) == 0x0a
+
+    # Once for shop, once for bag in either order
+    item_details.append(get_item_details(stream))
+    assert get_int(stream, 1) == 0x12
+    item_details.append(get_item_details(stream))
+
+    shop_items['items'] = item_details
+
+    # Store remaining cash
+    if get_int(stream, 1) == 0x18:
+        shop_items['cash_remaining'] = num_convert(stream)
+    else:
+        stream.seek(stream.tell()-1)
+
+    return shop_items
+
+def use(stream):
+    stream_length = num_convert(stream)
+    assert get_int(stream, 1) == 0x08
+    hp_restored = num_convert(stream)
+    return {
+        'length': stream_length,
+        'hp_restored': hp_restored
+    }
+
+
+def get_item_details(stream):
+    stream_length = num_convert(stream)
+
+    item_details = {}
+    storage = []
+    item_details['length'] = stream_length
+
+    stage = get_int(stream, 1)
+    if stage == 0x08:
+        item_details['name'] = LOCATION[get_int(stream, 1)]
+    else:
+        item_details['name'] = 'bag'
+        stream.seek(stream.tell()-1)
+
+    storage.append(get_items(stream))
+
+    item_details['storage'] = storage
+
+    return item_details
 
 def get_items(stream):
-    storage = []
+    storage = {}
 
     stage = get_int(stream, 1)
-    length = num_convert(stream)
+    #length = num_convert(stream)
     offset = stream.tell()
-    storage.append({'length': length})
+    #storage['length'] = length
 
-    location = stream.read(2)
-    if location == b'\x08\x02':
-        storage.append({'name': 'shop'})
-    else:
-        storage.append({'name': 'bag'})
-        stream.seek(stream.tell()-2)
-
-    stage = get_int(stream, 1)
     items = []
-    while stream.tell() - offset < length and stage == 0x12:
+    #while stream.tell() - offset < length and stage == 0x12:
+    while stage == 0x12:
         items.append(get_item(stream))
         stage = get_int(stream, 1)
 
-    storage.append({'items': items})
-    stream.seek(stream.tell()-1)
-    storage.append({'other': stream.read(length-(stream.tell()-offset))})
+    storage['items'] = items
+
+    # Store total slots
+    if stage == 0x18:
+        storage['slots'] = num_convert(stream)
+    else:
+        stream.seek(stream.tell()-1)
+
     return storage
 
 def get_item(stream):
-    item = []
+    item = {}
 
     length = num_convert(stream)
-    item.append({'length': length})
+    item['length'] = length
 
     next_attr = get_int(stream, 1)
     if next_attr == 0x08:
-        item.append({'lvl': get_int(stream,1)})
+        item['lvl'] = num_convert(stream)
         next_attr = get_int(stream, 1)
 
     if next_attr == 0x10:
-        item.append({'dmg': num_convert(stream)})
+        item['dmg'] = num_convert(stream)
         next_attr = get_int(stream, 1)
 
     if next_attr == 0x18:
-        item.append({'hp': num_convert(stream)})
+        item['hp'] = num_convert(stream)
         next_attr = get_int(stream, 1)
 
-    if next_attr != 0x22:
-        raise AssertionError(f"{hex(next_attr)} != 0x22")
-    #assert next_attr == 0x22
-    buy_at = []
+    assert next_attr == 0x22
+    buy_at = {}
     length = get_int(stream, 1)
-    buy_at.append({'length': length})
-    buy_at.append({'SOMETHING': stream.read(2)})
-    buy_at.append({'SOMETHING': stream.read(2)})
+    buy_at['length'] = length
+    buy_at['shop?'] = stream.read(2).hex()
+    buy_at['bag?'] = stream.read(2).hex()
 
     assert get_int(stream, 1) == 0x18
-    buy_at.append({'price': num_convert(stream)})
+    buy_at['price'] = num_convert(stream)
 
-    item.append({'buy_at': buy_at})
+    item['buy_at'] = buy_at
 
     assert get_int(stream, 1) == 0x22
-    sell_at = []
+    sell_at = {}
     length = get_int(stream, 1)
-    sell_at.append({'length': length})
-    sell_at.append({'SOMETHING': stream.read(2)})
+    sell_at['length'] = length
+    sell_at['shop?'] = stream.read(2).hex()
 
     if length <= 2:
+        item['sell_at'] = sell_at
         return item
 
     assert get_int(stream, 1) == 0x18
-    sell_at.append({'price': num_convert(stream)})
+    sell_at['price'] = num_convert(stream)
 
-    item.append({'sell_at': sell_at})
+    item['sell_at'] = sell_at
     return item
 
 
 def parse_attack(stream):
     stream_length = get_int(stream, 1)
 
-    attack_details = []
-    while get_int(stream, 1) == 0x0a:
+    attack_details = {}
+    attack_details['rounds'] = []
+    next_value = get_int(stream, 1)
+    while next_value == 0x0a:
         attack_length = get_int(stream, 1)
         assert get_int(stream, 1) == 0x08
         enemy_attack = num_convert(stream)
@@ -163,62 +263,49 @@ def parse_attack(stream):
         our_attack = num_convert(stream)
 
         # Store length plus initial byte plus last
-        attack_details.append({'attack': {'attack': our_attack, 'defend': enemy_attack}})
+        attack_details['rounds'].append({'attack': our_attack, 'defend': enemy_attack})
+        next_value = get_int(stream, 1)
 
-    if get_int(stream, 1) == 0x2a or len(attack_details) == 0:
+    if next_value == 0x2a:
         # Low HP
-        stream.seek(stream.tell()-2)
-        attack_details.extend(get_summary_details(stream))
+        stream.seek(stream.tell()-1)
+        attack_details.update(get_summary_details(stream))
     else:
         # Won the attack
-        stream.seek(stream.tell()-2)
-        attack_details.extend(get_win_details(stream))
+        stream.seek(stream.tell()-1)
+        attack_details.update(get_win_details(stream))
 
     return attack_details
 
 def get_win_details(stream):
 
     # Attack specific increases
-    attack_details = []
-    assert stream.read(2) == b'\x10\x01'
+    attack_details = {}
+    assert stream.read(1) == b'\x10'
+    attack_details['lvl'] = num_convert(stream)
     if get_int(stream, 1) == 0x18:
-        attack_details.append({'xp_increase': get_int(stream, 1)})
+        attack_details['xp_increase'] = num_convert(stream)
 
     if get_int(stream, 1) == 0x20:
-        attack_details.append({'gold_increase': get_int(stream, 1)})
+        attack_details['gold_increase'] = num_convert(stream)
 
-    attack_details.extend(get_summary_details(stream))
+    attack_details.update(get_summary_details(stream))
     return attack_details
 
 
 def get_summary_details(stream):
-    # Totals
-    attack_details = []
-    assert get_int(stream, 1) == 0x2a
-    summary_length = get_int(stream, 1)
+    summary_details = {}
+    stage = get_int(stream, 1)
+    assert stage == 0x2a or stage == 0x12
+    length = num_convert(stream)
+    offset = stream.tell()
 
-    assert get_int(stream, 1) == 0x08
-    attack_details.append({'lvl': get_int(stream, 1)})
+    summary_details['length'] = length
+    while stream.tell() - offset < length:
+        lookup = get_int(stream, 1)
+        summary_details[SUMMARY_LOOKUP[lookup]] = num_convert(stream)
 
-    if get_int(stream, 1) == 0x10:
-        attack_details.append({'xp': num_convert(stream)})
-
-    assert get_int(stream, 1) == 0x18
-    attack_details.append({'max_xp': num_convert(stream)})
-
-    assert get_int(stream, 1) == 0x20
-    attack_details.append({'another_lvl': num_convert(stream)})
-
-    assert get_int(stream, 1) == 0x28
-    attack_details.append({'hp': num_convert(stream)})
-
-    assert get_int(stream, 1) == 0x30
-    attack_details.append({'max_hp': num_convert(stream)})
-
-    assert get_int(stream, 1) == 0x38
-    attack_details.append({'gold': num_convert(stream)})
-
-    return attack_details
+    return summary_details
 
 
 # Get the token to send to the client
@@ -244,6 +331,14 @@ client.send(init)
 
 thekey = []
 output = []
+
+def signal_handler(sig, frame):
+    print("CTRL-C PRESSED")
+    server.close()
+    client.close()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
 
 # Send data to the client
 while True:
@@ -272,7 +367,12 @@ while True:
             print(hexdump.hexdump(bytes(decoded)))
             output.extend(decoded)
             if len(output) > 2:
-                print(parse(bytes(output)))
+                parsed = parse(bytes(output))
+                try:
+                    print(json.dumps(parsed, indent=2))
+                except Exception as ex:
+                    print(ex)
+                    print(parsed)
                 output = []
 
             client.send(buf)
